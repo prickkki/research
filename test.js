3ddf851305b1d5d6d6fb6325a7718bc3d8887a53
prickkki <59919866+prickkki@users.noreply.github.com>
2025-06-09 13:24:04 +0300

    Add files via upload
    init test

---
+++ test.js
@@ -0,0 +1,10 @@
+const http = require('http');
+http.get('httplocalhost3000', res = {
+  if (res.statusCode === 200) {
+    console.log('✅ OK');
+    process.exit(0);
+  } else {
+    console.error('❌ FAIL');
+    process.exit(1);
+  }
+});
